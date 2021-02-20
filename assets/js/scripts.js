efe31a703709d734edc84b2db54935778ad945cf
Damian Sieradzki <sieradzki129@gmail.com>
2021-02-20 14:55:06 +0100

    Second version

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,10 @@
     const addActive = () => {
     $("#first").click();
 }
+const peselValidation = () => {
+    const value = document.forms["covidRegister"]["pesel"].value
+    if (value > 11) {
+        alert("Błąd. Pole PESEL nie może być dłuższe niż 11 znaków.");
+        return false;
+    }
+}
